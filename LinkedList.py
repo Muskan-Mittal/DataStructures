db0ea6f724d1ae0c1bd9f30f13b82434a31b6cf1
Muskan-Mittal <muskanmtl10@gmail.com>
2017-12-01 11:09:45 +0530

    file updated with a new method to insert data at an index in the list

--- LinkedList.py
+++ LinkedList.py
@@ -58,6 +58,21 @@ class Linked_List(object):
 				return
 			current_index += 1
 
+	def insert_at_an_index(self, data, index):
+		if index >= self.length():
+			print "ERROR: Index out of range."
+			return
+		new_node = Node(data)
+		current_index = 0
+		current_node = self.head
+		while True:
+			current_node = current_node.next_node
+			if current_index is index:
+				new_node.next_node = current_node.next_node
+				current_node.next_node = new_node
+				return
+			current_index += 1
+
 
 if __name__ == '__main__':
 	my_list = Linked_List()
@@ -72,4 +87,8 @@ if __name__ == '__main__':
 
 	my_list.remove_at_an_index(2)
 	print "Element removed at index 2"
+	my_list.display()
+
+	my_list.insert_at_an_index(3, 0)
+	print "3 inserted at index 0"
 	my_list.display()
